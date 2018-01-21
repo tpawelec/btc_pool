8edf428bc6eae576d9af4742a9877e97fcb06d61
Tomasz Pawelec <30554524+lilinho@users.noreply.github.com>
2018-01-21 18:32:20 +0100

    chart fked up

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+/*
+TODO:
+zmienić dataset - te duże liczby to timestamp
+zmienić timestamp na zwykłą datę
+*/
+
 /*jslint browser: true*/ /*global  $, window*/
 /*jshint sub:true*/
 
@@ -66,47 +72,34 @@ function drawChart(resp) {
         }
         i++;
     }
-    console.log(datasets[0]);
+    
+    var min = Math.min(...datasets[0]);
+    console.log(min);
     var myChart = new Chart(chartCanvas, {
     type: 'line',
     data: {
         labels: datasets[0],
         datasets: [{
             data: datasets[0],
-            backgroundColor: [
-                'rgba(255, 99, 0, 0.2)',
-            ],
-            borderColor: [
-                'rgba(0,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            borderColor: 'rgba(0,99,132,1)',
+            borderWidth: 1,
+            pointBackgroundColor: 'rgba(0,99,132,1)'
         },
         {
             data: datasets[1],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-            ],
-            borderColor: [
-                'rgba(34,99,132,1)',
-                'rgba(10, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(2, 102, 255, 1)',
-                'rgba(45, 159, 64, 1)'
-            ],
+            borderColor: 'rgba(34,99,132,1)',
             borderWidth: 1
         }]
     },
     options: {
+        responsive: true,
+        maintainAspectRatio: false,
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero:true
+                    beginAtZero:true,
+                    autoSkip: true,
+                    min: 1500000000 
                 }
             }],
             xAxes: [{
