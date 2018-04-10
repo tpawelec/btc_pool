bdc0ac62d5e93fa250c50dd3248324d5c0bc095d
Tomasz Pawelec <tpawelec@outlook.com>
2018-04-10 16:58:57 +0200

    password confirm tooltip

--- js/settings-script.js
+++ js/settings-script.js
@@ -1,7 +1,17 @@
 $(document).ready(function() {
+
+	var $tooltip = $('.col-tooltip');
+	var duration = 500;
+	$tooltip.hide();
 	$('#newPasswordConfirm').focusout(function() {
-		if($(this).val() === $('#newPassword').val()) {
-			console.log("tosamo")
+		if($(this).val() != $('#newPassword').val()) {
+			$tooltip.slideDown(duration);
+			$(this).focus()
 		}
-	})
-})
+	});
+
+	$('#newPasswordConfirm').on('keyup', function() {
+			$tooltip.slideUp(duration);
+		}
+	);
+});
